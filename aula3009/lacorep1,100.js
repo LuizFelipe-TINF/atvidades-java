e09ee163b331f74b29576223bb9ea8e680ad5a2a
LuizFelipe-TINF <lfrsilv@gmail.com>
2025-09-30 21:10:03 -0300

    Add files via upload

--- aula3009/lacorep1,100.js
+++ aula3009/lacorep1,100.js
@@ -1,7 +1,4 @@
 
-
-
-
 function alterarMSG(nome){
     const elemento = document.getElementById("msg")
     elemento.innerHTML = `Boa tarde ${nome}!`
@@ -12,14 +9,16 @@ function alterarMSG(nome){
     elementoBotao.addEventListener("click", function(){
         
        const elemento = document.getElementById("msg")
-       const nome = document.getElementById("nome").value
-       console.log(nome)
-        elemento.innerHTML = `Boa tarde ${nome}!`
-        for(i = 0; i >= 100; i++ ){
 
-            document.getElementById("numero")
-            elemento.innerHTML = (numero)
-        
-        }
+       const nome = document.getElementById("nome").value
+
+       console.log(nome)
+
+        elemento.innerHTML = `Boa tarde ${nome}!`
+
+        let numerosElemento = document.getElementById("numeros");
+        for (let i = 1; i <= 100; i++) {
+        numerosElemento.innerHTML += i + " "; //" " para adicionar um espaço entre os números, faz virar string
+    }
     
     })
